f3855959728f44529987e3e2db591efd6e56b03c
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2024-04-23 07:58:44 +0200

    Configured object storage with rados rather than minioGW

--- jhub/envs-setup.sh
+++ jhub/envs-setup.sh
@@ -38,8 +38,8 @@
 ################################################################################
 ## Setup Cloud Storage
 ## -------------------
-## This storage is cloud-persistent and relies on the managed MinIO service,
-## for details, please refer to minio.cloud.infn.it.
+## This storage is cloud-persistent and relies on the managed RadosGW service,
+## for details, please refer to rgw.cloud.infn.it.
 ## This storage is good for medium-size datasets shared among multiple users.
 ## It does not support symbolic links and it becomes slow with a large amount
 ## of small files. Hence, it is not good for sharing code and software 
@@ -49,30 +49,35 @@
 BASE_CACHE_DIR="/usr/local/share/dodasts/sts-wire/cache"
 WORKAREA="/home"
 
+####
+# Temporary solution to migrate from minio to rados GW: cached rclone must be replaced.
+rm -f /home/private/.cache/sts-wire/rclone
+####
+
 mkdir -p "${BASE_CACHE_DIR}"
 mkdir -p /usr/local/share/dodasts/sts-wire/cache
 mkdir -p /var/log/sts-wire/
 mkdir -p /s3/
-mkdir -p "${WORKAREA}"/minio
+mkdir -p "${WORKAREA}"/rgw
 
-function mount_minio {
+function mount_rgw {
   local volume=$1;
   mkdir -p /s3/$volume;
   nice -n 19 sts-wire https://iam.cloud.infn.it/ \
-      "$volume" https://minio.cloud.infn.it/ \
+      "$volume" https://rgw.cloud.infn.it/ IAMaccess object \
       "/$volume" "/s3/$volume" \
       --localCache full --tryRemount --noDummyFileCheck \
       --localCacheDir "${BASE_CACHE_DIR}/$volume" \
       &>"/var/log/sts-wire/mount_log_$volume.txt" &
-  ln -s "/s3/$volume" "${WORKAREA}/minio/$volume"
+  ln -s "/s3/$volume" "${WORKAREA}/rgw/$volume"
 }
 
-sleep 0.1 && mount_minio $USERNAME || echo "Ok"  
-sleep 0.2 && mount_minio scratch || echo "Ok"
+sleep 0.1 && mount_rgw $USERNAME || echo "Ok"  
+sleep 0.2 && mount_rgw scratch || echo "Ok"
 
 for additional_volume in $@;
 do
-  mount_minio $additional_volume || echo "Ok"  ;
+  mount_rgw $additional_volume || echo "Ok"  ;
 done
 
 ################################################################################
